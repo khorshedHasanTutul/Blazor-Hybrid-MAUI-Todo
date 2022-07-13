9025097fd61222a1b0edd66a5c4afffa7db0aef6
khorshedHasanTutul <khasan7890@gmail.com>
2022-07-13 16:27:37 +0600

    todo list using maui blazor & blazor server

---
+++ RazorLibrary/Models/TodoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RazorLibrary.Models
+{
+    public class TodoModel
+    {
+        public string TodoItem { get; set; } = String.Empty;
+        public bool IsComplete { get; set; }
+    }
+}
